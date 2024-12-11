134db2163daf343a568a06e500f44582e74c9dee
Michael Lee <michaellee@Michaels-MBP.attlocal.net>
2024-12-10 18:22:34 -0600

    Pass in question set from app level

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { GameEnd } from "./modules/end/End";
 import { Game } from "./modules/game/Game";
+import { QUESTIONS } from "./modules/game/questions";
 import { Home } from "./modules/home/Home";
 import { Settings } from "./modules/settings/Settings";
 import { SettingValues } from "./modules/settings/type";
@@ -15,6 +16,7 @@ enum GameState {
 export const App = () => {
   const [gameState, setGameState] = useState<GameState>(GameState.START);
   const [winningTeam, setWinningTeam] = useState<{ score: number, team: number }>();
+  const [questionSet] = useState(QUESTIONS);
   const [targetScore, setTargetScore] = useState(300);
 
   const handleGameEnd = (team: number, score: number) => {
@@ -28,13 +30,36 @@ export const App = () => {
 
   switch (gameState) {
     case GameState.PROGRESS:
-      return <Game onEnd={handleGameEnd} targetPoints={targetScore} />;
+      return (
+        <Game
+          onEnd={handleGameEnd}
+          questions={questionSet}
+          targetPoints={targetScore}
+        />
+      );
     case GameState.END:
-      return <GameEnd onRestart={() => setGameState(GameState.PROGRESS)} team={winningTeam?.team || 0} score={winningTeam?.score || 0} />;
+      return (
+        <GameEnd
+          onRestart={() => setGameState(GameState.PROGRESS)}
+          team={winningTeam?.team || 0}
+          score={winningTeam?.score || 0}
+        />
+      );
     case GameState.SETTINGS:
-      return <Settings initial={{ targetScore }} onCancel={() => setGameState(GameState.START)} onSubmit={updateSettings} />
+      return (
+        <Settings
+          initial={{ targetScore }}
+          onCancel={() => setGameState(GameState.START)}
+          onSubmit={updateSettings}
+        />
+      );
     case GameState.START:
     default:
-      return <Home onStart={() => setGameState(GameState.PROGRESS)} onSettings={() => setGameState(GameState.SETTINGS)} />;
+      return (
+        <Home
+          onStart={() => setGameState(GameState.PROGRESS)}
+          onSettings={() => setGameState(GameState.SETTINGS)}
+        />
+      );
   }
 };
